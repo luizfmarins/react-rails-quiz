1aca0476b9d4091e69ed50c5eb8fbcd7c002a10f
luiz.marins <luiz.f.marins@gmail.com>
2017-10-21 16:46:50 -0200

    Alow user to select an option

--- app/assets/javascripts/react_components/_option_list.js.jsx
+++ app/assets/javascripts/react_components/_option_list.js.jsx
@@ -1,17 +1,41 @@
 var OptionList = React.createClass({
+
   getInitialState: function () {
-    return JSON.parse(this.props.presenter);
+    var state = JSON.parse(this.props.presenter);
+    state.selectedOption = "";
+    return state;
+  },
+
+  handleOptionChange: function (changeEvent) {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
+  },
+
+  getSelectedOption: function () {
+    return this.state.selectedOption;
+  },
+
+  handleSubmit: function (event) {
+    event.preventDefault();
+
+    console.log('You have selected:', this.state.selectedOption);
   },
 
   render: function () {
+    var self = this;
+
     var optionNodes = this.state.quizOptions.map(function (op) {
-      return <QuizOption opId={op.id} text={op.text}/>
+      return <QuizOption opId={op.id} text={op.text}
+                         handleOptionChange={self.handleOptionChange}
+                         getSelectedOption={self.getSelectedOption}/>
     });
 
     return (
       <div>
-        <form action="">
+        <form ref="form" method="post" onSubmit={this.handleSubmit}>
           {optionNodes}
+          <button type="submit">Next</button>
         </form>
       </div>
     )
--- app/assets/javascripts/react_components/_quiz_option.js.jsx
+++ app/assets/javascripts/react_components/_quiz_option.js.jsx
@@ -2,7 +2,11 @@ var QuizOption = React.createClass({
   render: function () {
     return (
       <div>
-        <input type="radio" name = "quizOption" value={this.props.opId}>{this.props.text}</input>
+        <input type="radio" name="quizOption" value={this.props.opId}
+               checked={this.props.getSelectedOption() === this.props.opId}
+               onChange={this.props.handleOptionChange}>
+          {this.props.text}
+        </input>
       </div>
     )
   }
