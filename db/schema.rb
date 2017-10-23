19a6481992ae7f810985479502ecdbce26f22f79
luiz.marins <luiz.f.marins@gmail.com>
2017-10-22 22:56:28 -0200

    Delete example files from base project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315030553) do
+ActiveRecord::Schema.define(version: 201710222114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,13 +21,6 @@ ActiveRecord::Schema.define(version: 20140315030553) do
   end
 
   add_index "answers", ["option_id"], name: "index_answers_on_option_id", using: :btree
-
-  create_table "comments", force: true do |t|
-    t.string   "author"
-    t.text     "text"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "options", force: true do |t|
     t.integer "question_id"
